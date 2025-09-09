8227f0026a987c3a0e8b839f461164be273071f5
pixaut <116499977+pixaut@users.noreply.github.com>
2025-09-09 14:10:32 +0300

    Update lab1-3.py

--- lab1-3.py
+++ lab1-3.py
@@ -4,6 +4,6 @@ result = 1
 for i in range(1,len(numbers),2):
     result *= numbers[i] 
 
-numbers.remove(max(numbers))
 print(f'Произведение чисел на чётных номерах: {result}')
+
 print(f'Длина списка: {len(numbers)}' )
